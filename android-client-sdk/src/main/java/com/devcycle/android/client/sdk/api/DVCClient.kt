5a0bfc323918e151518f4e8ad29d7e61716a2d0b
Nic <nic@randomidea.org>
2021-12-07 13:53:49 -0800

    DVC-1420 base SDK with init (fetch config) and callback (#2)
    * DVC-1420 base SDK with init (fetch config) and callback
    
    * Handle non success when requesting config
    
    * Remove file
    
    * Clean gitignore
    
    * Removed app specific resources

---
+++ android-client-sdk/src/main/java/com/devcycle/android/client/sdk/api/DVCClient.kt
@@ -0,0 +1,93 @@
+package com.devcycle.android.client.sdk.api
+
+import com.devcycle.android.client.sdk.model.*
+
+class DVCClient private constructor(
+    private val environmentKey: String,
+    private val user: User,
+    options: DVCOptions?
+) {
+    private val request: Request = Request()
+    private var config: BucketedUserConfig? = null
+    private fun saveUser() {
+        throw NotImplementedError()
+    }
+
+    fun initialize(callback: DVCCallback<String?>) {
+        request.getConfigJson(environmentKey, user, object : DVCCallback<BucketedUserConfig?> {
+            override fun onSuccess(result: BucketedUserConfig?) {
+                config = result
+                callback.onSuccess("Config loaded")
+            }
+
+            override fun onError(t: Throwable?) {
+                callback.onError(t)
+            }
+        })
+    }
+
+    fun identifyUser(): Variable {
+        throw NotImplementedError()
+    }
+
+    fun resetUser() {
+        throw NotImplementedError()
+    }
+
+    fun allFeatures(): Map<String, Feature> {
+        throw NotImplementedError()
+    }
+
+    fun allVariables(): Map<String, Feature> {
+        throw NotImplementedError()
+    }
+
+    fun <T> variable(key: String?, defaultValue: T): Variable {
+        throw NotImplementedError()
+    }
+
+    fun track(): DVCResponse {
+        throw NotImplementedError()
+    }
+
+    fun flushEvents() {
+        throw NotImplementedError()
+    }
+
+    fun fetchConfig() {
+        throw NotImplementedError()
+    }
+
+    class DVCClientBuilder {
+        private var environmentKey: String? = null
+        private var user: User? = null
+        private var options: DVCOptions? = null
+        fun environmentKey(environmentKey: String?): DVCClientBuilder {
+            this.environmentKey = environmentKey
+            return this
+        }
+
+        fun user(user: User?): DVCClientBuilder {
+            this.user = user
+            return this
+        }
+
+        fun options(options: DVCOptions?): DVCClientBuilder {
+            this.options = options
+            return this
+        }
+
+        fun build(): DVCClient {
+            require(!(environmentKey == null || environmentKey == "")) { "SDK key must be set" }
+            requireNotNull(user) { "User must be set" }
+            return DVCClient(environmentKey!!, user!!, options)
+        }
+    }
+
+    companion object {
+        fun builder(): DVCClientBuilder {
+            return DVCClientBuilder()
+        }
+    }
+
+}
---
+++ android-client-sdk/src/main/java/com/devcycle/android/client/sdk/api/Request.kt
@@ -0,0 +1,66 @@
+package com.devcycle.android.client.sdk.api
+
+import com.devcycle.android.client.sdk.exception.DVCException
+import com.devcycle.android.client.sdk.model.BucketedUserConfig
+import com.devcycle.android.client.sdk.model.ErrorResponse
+import com.devcycle.android.client.sdk.model.HttpResponseCode
+import com.devcycle.android.client.sdk.model.User
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
+internal class Request {
+    private var api: DVCApi? = null
+    private val objectMapper = ObjectMapper()
+    @Synchronized
+    fun getConfigJson(
+        environmentKey: String?,
+        user: User?,
+        callback: DVCCallback<BucketedUserConfig?>
+    ) {
+        val map =
+            objectMapper.convertValue(user, object : TypeReference<Map<String?, String?>?>() {})
+        val call = api!!.getConfigJson(environmentKey, map)
+        call!!.enqueue(object : Callback<BucketedUserConfig?> {
+            override fun onResponse(
+                call: Call<BucketedUserConfig?>,
+                response: Response<BucketedUserConfig?>
+            ) {
+                if (response.isSuccessful) {
+                    val config = response.body()
+                    callback.onSuccess(config)
+                } else {
+                    val httpResponseCode = HttpResponseCode.byCode(response.code())
+                    var errorResponse = ErrorResponse("Unknown Error", null)
+                    var dvcException = DVCException(httpResponseCode, errorResponse)
+                    if (response.errorBody() != null) {
+                        try {
+                            errorResponse = objectMapper.readValue(
+                                response.errorBody()!!.string(),
+                                ErrorResponse::class.java
+                            )
+                            dvcException = DVCException(httpResponseCode, errorResponse)
+                        } catch (e: IOException) {
+                            errorResponse.message = e.message
+                            dvcException = DVCException(httpResponseCode, errorResponse)
+                        }
+                    }
+                    callback.onError(dvcException)
+                }
+            }
+
+            override fun onFailure(call: Call<BucketedUserConfig?>, t: Throwable) {
+                callback.onError(t)
+            }
+        })
+    }
+
+    init {
+        api = DVCApiClient().initialize()
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+    }
+}
