b5a04efb8eb7d72a266dd13fa2bfd71700687e74
Jason Salaber <jcsalaber@hotmail.com>
2022-04-27 11:37:12 -0400

    fix: ignore json properties that are unknown

--- android-client-sdk/src/main/java/com/devcycle/sdk/android/model/BucketedUserConfig.kt
+++ android-client-sdk/src/main/java/com/devcycle/sdk/android/model/BucketedUserConfig.kt
@@ -11,6 +11,7 @@
  */
 package com.devcycle.sdk.android.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 
@@ -18,6 +19,7 @@ import java.math.BigDecimal
  * ClientSDKAPIResponse
  */
 data class BucketedUserConfig internal constructor(
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @get:Schema(required = true, description = "")
     val project: Project? = null,
     @get:Schema(required = true, description = "")
