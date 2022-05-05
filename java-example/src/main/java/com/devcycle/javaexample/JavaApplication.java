cf7000a552a225a8316ec1b428731d486ad4c0ab
Jason Salaber <jcsalaber@hotmail.com>
2022-05-05 15:22:09 -0400

    chore: added application class to examples for proper installation location of devcycle

---
+++ java-example/src/main/java/com/devcycle/javaexample/JavaApplication.java
@@ -0,0 +1,76 @@
+package com.devcycle.javaexample;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.devcycle.sdk.android.api.DVCCallback;
+import com.devcycle.sdk.android.api.DVCClient;
+import com.devcycle.sdk.android.model.DVCEvent;
+import com.devcycle.sdk.android.model.DVCUser;
+import com.devcycle.sdk.android.model.Variable;
+import com.devcycle.sdk.android.util.LogLevel;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Objects;
+
+public class JavaApplication extends Application {
+    Variable<String> variable = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DVCClient client = DVCClient.builder()
+                .withContext(getApplicationContext())
+                .withUser(
+                        DVCUser.builder()
+                                .withUserId("test_user")
+                                .build()
+                )
+                .withEnvironmentKey("<ADD-MOBILE-KEY-HERE>")
+                .withLogLevel(LogLevel.DEBUG)
+                .build();
+
+        // Use your own demo variable here to see the value change from the defaultValue when the client is initialized
+        variable = client.variable("<YOUR_VARIABLE_KEY>", "my string variable is not initialized yet");
+        Toast.makeText(getApplicationContext(), Objects.requireNonNull(variable.getValue()), Toast.LENGTH_SHORT).show();
+
+        client.onInitialized(new DVCCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+
+                client.flushEvents(new DVCCallback<String>() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable t) {
+                        Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                DVCEvent event = DVCEvent.builder()
+                        .withType("custom_event_type")
+                        .withTarget("custom_event_target")
+                        .withValue(BigDecimal.valueOf(10.00))
+                        .withMetaData(Collections.singletonMap("test", "value"))
+                        .build();
+                client.track(event);
+
+                // This toast onInitialized will show the value has changed
+                Toast.makeText(getApplicationContext(), variable.getValue(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable t) {
+                Toast.makeText(getApplicationContext(), "Client did not initialize: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- java-example/src/main/java/com/devcycle/javaexample/MainActivity.java
+++ java-example/src/main/java/com/devcycle/javaexample/MainActivity.java
@@ -18,61 +18,8 @@ import java.util.Collections;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    Variable<String> variable = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        DVCClient client = DVCClient.builder()
-                .withContext(getApplicationContext())
-                .withUser(
-                        DVCUser.builder()
-                                .withUserId("test_user")
-                                .build()
-                )
-                .withEnvironmentKey("<ADD-MOBILE-KEY-HERE>")
-                .withLogLevel(LogLevel.DEBUG)
-                .build();
-
-        // Use your own demo variable here to see the value change from the defaultValue when the client is initialized
-        variable = client.variable("<YOUR_VARIABLE_KEY>", "my string variable is not initialized yet");
-        Toast.makeText(MainActivity.this, Objects.requireNonNull(variable.getValue()), Toast.LENGTH_SHORT).show();
-
-        client.onInitialized(new DVCCallback<String>() {
-            @Override
-            public void onSuccess(String result) {
-                //Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-
-                client.flushEvents(new DVCCallback<String>() {
-                    @Override
-                    public void onSuccess(String result) {
-                        Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                DVCEvent event = DVCEvent.builder()
-                        .withType("custom_event_type")
-                        .withTarget("custom_event_target")
-                        .withValue(BigDecimal.valueOf(10.00))
-                        .withMetaData(Collections.singletonMap("test", "value"))
-                        .build();
-                client.track(event);
-
-                // This toast onInitialized will show the value has changed
-                Toast.makeText(MainActivity.this, variable.getValue(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(@NonNull Throwable t) {
-                Toast.makeText(MainActivity.this, "Client did not initialize: " + t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
