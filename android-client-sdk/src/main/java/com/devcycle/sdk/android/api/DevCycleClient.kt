a7f798e57eebbff520845688ac8b172ac9eb1dd6
Jonathan Norris <jonathan@taplytics.com>
2023-07-20 15:24:43 -0400

    feat: update version to 2.0.0

--- android-client-sdk/src/main/java/com/devcycle/sdk/android/api/DevCycleClient.kt
+++ android-client-sdk/src/main/java/com/devcycle/sdk/android/api/DevCycleClient.kt
@@ -31,7 +31,7 @@ import kotlin.coroutines.CoroutineContext
  * All methods that make requests to the APIs or access [config] and [user] are [Synchronized] to
  * ensure thread-safety
  */
-class DevCycleClient private constructor(
+open class DevCycleClient protected constructor(
     private val context: Context,
     private val sdkKey: String,
     private var user: PopulatedUser,
