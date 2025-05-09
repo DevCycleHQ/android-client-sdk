c6b8063909186da31f706f92dd39023a3872a9f4
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2025-05-09 10:04:30 -0400

    fix: allow JSON deserialization of the DevCycle User Config to handle new properties

--- android-client-sdk/src/main/java/com/devcycle/sdk/android/util/JSONMapper.kt
+++ android-client-sdk/src/main/java/com/devcycle/sdk/android/util/JSONMapper.kt
@@ -1,8 +1,11 @@
 package com.devcycle.sdk.android.util
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 object JSONMapper {
-    val mapper = jacksonObjectMapper().registerModule(JsonOrgModule())
+    val mapper = jacksonObjectMapper()
+        .registerModule(JsonOrgModule())
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
