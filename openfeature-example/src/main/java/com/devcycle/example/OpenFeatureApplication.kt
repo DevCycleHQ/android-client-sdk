8d078db3fc4cb18d325a5943ea13b00e91973d1b
Jonathan Norris <jonathan@taplytics.com>
2025-07-07 11:47:30 -0400

    chore: change key name

--- openfeature-example/src/main/java/com/devcycle/example/OpenFeatureApplication.kt
+++ openfeature-example/src/main/java/com/devcycle/example/OpenFeatureApplication.kt
@@ -41,7 +41,7 @@ class OpenFeatureApplication: Application() {
                     .build()
                     
                 val provider = DevCycleProvider(
-                    sdkKey = "YOUR_DEVCYCLE_MOBILE_KEY",
+                    sdkKey = "<DEVCYCLE_MOBILE_SDK_KEY>",
                     context = applicationContext,
                     options = options
                 )
